8e3ba9c68b2a6603bbadbff7c605cf8e77f268a5
gokhanyilmaz <gokhanyilmaz>
2019-10-24 23:01:59 +0300

    with NodeJS Express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody on IBM Cloud!");
+  res.send("Hello from Appsody with NodeJS Express on IBM Cloud!");
 });
  
 module.exports.app = app;
